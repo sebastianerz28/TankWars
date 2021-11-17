01dfc32cb0b016ff338250adee9fa2318aba28e3
GRANT NATIONS <u1247784@umail.utah.edu>
2021-11-16 20:24:42 -0700

    write recieve startup callback

--- TankWars/GameController/Controller.cs
+++ TankWars/GameController/Controller.cs
@@ -1,24 +1,91 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using NetworkUtil;
 
 namespace GameController
 {
     public class Controller
     {
-        public delegate void ErrorOccuredHandler(string ErrorMessage); 
+        private string playerName;
+        private int id;
+        private int worldSize;
+        private bool idInitialized = false;
+        private bool worldSizeInitialized = false;
+
+        public delegate void ErrorOccuredHandler(string ErrorMessage);
         public void Connect(string hostName, string playerName)
         {
+            this.playerName = playerName;
             Networking.ConnectToServer(FirstContact, hostName, 11000);
         }
 
-        public void FirstContact(SocketState socket)
+        private void FirstContact(SocketState state)
         {
-            if(socket.ErrorOccurred == true)
+            if (state.ErrorOccurred == true)
             {
-                ErrorOccurred(socket.ErrorMessage);
+                ErrorOccurred(state.ErrorMessage);
+                return;
             }
 
+            state.OnNetworkAction = ReceiveStartup;
+            Networking.Send(state.TheSocket, playerName + "\n");
+            Networking.GetData(state);
         }
+
+        private void ReceiveWorld(SocketState state)
+        {
+            //DO something
+        }
+
+        private void ReceiveStartup(SocketState state)
+        {
+            string totalData = state.GetData();
+            string[] parts = Regex.Split(totalData, @"(?<=[\n])"); //, RegexOptions.IgnorePatternWhitespace
+
+            if (parts.Length < 2 || !totalData.EndsWith("\n"))
+            {
+                Networking.GetData(state);
+                return;
+            }
+            else
+            {
+                id = int.Parse(parts[0]);
+                worldSize = int.Parse(parts[1]);
+                state.RemoveData(0, totalData.Length);
+                state.OnNetworkAction = ReceiveWorld;
+                Networking.GetData(state);
+            }
+
+            //foreach (string p in parts)
+            //{
+            //    // Ignore empty strings added by the regex splitter
+            //    if (p.Length == 0)
+            //        continue;
+
+            //    if (int.TryParse(p, out int val))
+            //    {
+            //        if (!idInitialized)
+            //        {
+            //            id = val;
+            //        }
+            //        else if (!worldSizeInitialized)
+            //        {
+            //            worldSize = val;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Networking.GetData(state);
+            //        return;
+            //    }
+
+            //// Then remove it from the SocketState's growable buffer
+            //state.RemoveData(0, p.Length);
+            //}
+
+
+        }
+
         public event ErrorOccuredHandler ErrorOccurred;
     }
 }
